b1044428d47555ffdd8739ba3f199837a5965bf3
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2022-08-12 10:29:49 +0300

    feat lesson01 more main.js

--- Readme.md
+++ Readme.md
@@ -1,6 +1,6 @@
 #  ME_JavaScript_Lessons 
 for Others Lessons of **Module2**
 
-https://github.com/mindfling/ME_JavaScript_Lessons
-https://github.com/mindfling/ME_JavaScript_Lessons/tree/m2lesson01/m2lesson01
+- https://github.com/mindfling/ME_JavaScript_Lessons
+- https://github.com/mindfling/ME_JavaScript_Lessons/tree/m2lesson01/m2lesson01
 
--- m2lesson01/js/main.js
+++ m2lesson01/js/main.js
@@ -20,68 +20,51 @@ const five = items.querySelector('.item_five');
 const six = items.querySelector('.item_six');
 
 
-// *****
-// todo two
+// two
 const twoTitle = two.querySelector('.item__title');
 const twoTitleText = twoTitle.textContent;
-
 const twoPropsList = two.querySelector('.props__list');
-console.log('twoPropsList: ', twoPropsList);
 const propsItemFour = twoPropsList.querySelector('.props__item_four');
-console.log('propsItemFour: ', propsItemFour);
+// propsItemFour.remove();
 
-propsItemFour.remove();
-
-// todo three
+// three
 const threeTitle = three.querySelector('.item__title');
-const threeTitleText = threeTitle.textContent;
+// const threeTitleText = threeTitle.textContent;
 const threePropsList = three.querySelector('.props__list');
 // threePropsList.remove();
 
-// todo five
+// five
 const fiveTitle = five.querySelector('.item__title');
 const fiveTitleText = fiveTitle.textContent;
 const fivePropsList = five.querySelector('.props__list');
 // fivePropsList.remove();
 threeTitle.after(fivePropsList);
 
-// todo six
+// six
 const sixTitle = six.querySelector('.item__title');
 const sixTitleText = sixTitle.textContent;
 const sixPropsItemTwoAll = six.querySelectorAll('.props__item_two');
-console.log('sixPropsItemTwoAll: ', sixPropsItemTwoAll);
 sixPropsItemTwoAll.forEach((propsItem) => {
   twoPropsList.append(propsItem);
 });
 
-// todo four
+// four
 const fourPropsList = four.querySelector('.props__list');
-console.log('fourPropsList: ', fourPropsList);
 const fourPropsItemAll = fourPropsList.querySelectorAll('.props__item');
-console.log('fourPropsItemAll [2]: ', fourPropsItemAll[2]);
 fourPropsItemAll[2].after(propsItemFour);
 
-
 twoTitle.textContent = fiveTitleText;
-threeTitle.textContent = 'This и прототипы объектов'; // todo
+threeTitle.textContent = 'This и прототипы объектов';
 fiveTitle.textContent = sixTitleText;
 sixTitle.textContent = twoTitleText;
 
 fiveTitle.after(threePropsList);
-console.log('fiveTitle: ', fiveTitle);
 
 
-// ** ТЕПЕРЬ В ПРАВИЛЬНОМ ПОРЯДКЕ **
-// remove all items in forEach
+// ** Теперь в правильном порядке **
 itemAll.forEach((item) => {
   item.remove();
 });
 
-// в правильном порядке
-items.append(one);
-items.append(two);
-items.append(three);
-items.append(four);
-items.append(five);
-items.append(six);
+items.append(one, two, three, four, five, six);
 
